f48e2065ffe6d8e75e51fd604b98a500c5632ea2
Ardhi1209 <aadityarizaldi@gmail.com>
2017-03-20 23:06:15 +0700

    Add files via upload

---
+++ mod1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+//contoh modular1
+
+int main () {
+	// variabel
+	int a, b, temp;
+	
+	//proses membaca
+	cout << "masukkan nilai A : ";
+	cin >> a;
+	
+	cout << "masukkan nilai b : ";
+	cin >> b;
+	
+	//proses tukar
+	temp = b;
+	b = a;
+	a = temp;
+	
+	//proses tampil
+	cout << "============================" << endl;
+	cout << "nilai A sekarang : " << a << endl;
+	cout << "nilai B sekarang : " << b;
+	
+	return 0;
+	}
